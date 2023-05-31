851b6af1ff99d0b2b76a157e60f846205bf1bc7c
Lumart Chryssomallis <lumartch@gmail.com>
2023-05-31 12:56:37 -0600

    Updated test to avoid conflicts on the build.

--- src/___tests___/services/ProductService.test.ts
+++ src/___tests___/services/ProductService.test.ts
@@ -21,27 +21,32 @@ describe('ProductService Unit Test', () => {
             description: chance.string(),
         }
         updatedProduct = productData;
+        // @ts-expect-error
         global.console = { log: jest.fn(), error: jest.fn() }
         ProductModel.find = jest.fn().mockReturnThis();
         ProductModel.findById = jest.fn().mockReturnThis();
         productModel.save = jest.fn().mockReturnValue(productData);
         ProductModel.findByIdAndUpdate = jest.fn().mockReturnThis();
         ProductModel.findByIdAndDelete = jest.fn().mockReturnThis();
+        // @ts-expect-error
         ProductModel.lean = jest.fn().mockReturnThis();
     })
     it('should return all products', async() => {
+        // @ts-expect-error
         ProductModel.exec = jest.fn().mockResolvedValue([productData]);
         const data = await productService.findAllProducts();
         expect(data.length).toBe(1);
     });
 
     it('should return a product by ID', async() => {
+        // @ts-expect-error
         ProductModel.exec = jest.fn().mockResolvedValue(productData);
         await productService.findProductById(id);
         expect(ProductModel.findById).toBeCalledWith(id);
     });
 
     it('should return an updated product', async() => {
+        // @ts-expect-error
         ProductModel.exec = jest.fn().mockResolvedValue(productData);
         const data = await productService.updateProduct(id, productData);
         expect(data).toEqual(updatedProduct)
